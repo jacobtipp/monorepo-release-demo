de33bfbb458ea549d85004abc7f4d58c01e1b48a
Jacob Tipp <jacobtipp@gmail.com>
2023-04-20 22:38:02 +0000

    refactor(monorepo-release-demo-b): ♻️ update addThree

--- packages/monorepo-release-demo-b/src/lib/add-three.ts
+++ packages/monorepo-release-demo-b/src/lib/add-three.ts
@@ -1,3 +1,3 @@
 import { addTwo } from '@jacobtipp/monorepo-release-demo-a';
 
-export const addThree = (numn: number) => addTwo(numn) + 1;
+export const addThree = (num: number) => addTwo(num) + 1;
