c62599f6558170cd8019ccd63a66fd5a1bb94d6f
jacobtipp <jacobtipp@gmail.com>
2023-05-12 17:19:58 -0700

    fix(monorepo-release-demo-b): 🐛 update addThree arguments

--- packages/monorepo-release-demo-b/src/lib/add-three.ts
+++ packages/monorepo-release-demo-b/src/lib/add-three.ts
@@ -3,4 +3,4 @@ import { addTwo } from '@jacobtipp/monorepo-release-demo-a';
 console.log(addTwo);
 console.log('test');
 
-export const addThree = (numm: number) => addTwo(numm) + 1;
+export const addThree = (num: number) => addTwo(num) + 1;
